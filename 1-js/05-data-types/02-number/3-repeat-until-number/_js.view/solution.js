b1de3f1d6964a7c4f80dd82eb12fba4a01c4edfc
I_am_Vietnam <91591390+ImVietnam@users.noreply.github.com>
2023-03-04 12:31:22 +0700

    Update solution.js

--- 1-js/05-data-types/02-number/3-repeat-until-number/_js.view/solution.js
+++ 1-js/05-data-types/02-number/3-repeat-until-number/_js.view/solution.js
@@ -3,7 +3,7 @@ function readNumber() {
   let num;
 
   do {
-    num = prompt("Enter a number please?", 0);
+    num = prompt("Vui lòng nhập số?", 0);
   } while ( !isFinite(num) );
 
   if (num === null || num === '') return null;
