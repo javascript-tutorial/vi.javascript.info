bd538f2119d2f7b915bd1c3a362741b1915ce59d
I_am_Vietnam <91591390+ImVietnam@users.noreply.github.com>
2023-06-05 21:02:36 +0700

    Update source.js

--- 1-js/06-advanced-functions/06-function-object/2-counter-inc-dec/_js.view/source.js
+++ 1-js/06-advanced-functions/06-function-object/2-counter-inc-dec/_js.view/source.js
@@ -1,7 +1,7 @@
 function makeCounter() {
   let count = 0;
 
-  // ... your code ...
+  // ... mã của bạn ...
 }
 
 let counter = makeCounter();
@@ -9,10 +9,10 @@ let counter = makeCounter();
 alert( counter() ); // 0
 alert( counter() ); // 1
 
-counter.set(10); // set the new count
+counter.set(10); // đặt số lượng đếm mới
 
 alert( counter() ); // 10
 
-counter.decrease(); // decrease the count by 1
+counter.decrease(); // giảm số lượng đi 1
 
-alert( counter() ); // 10 (instead of 11)
+alert( counter() ); // 10 (thay vì 11)
