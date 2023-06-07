1b8289fba03401672ced664ff556a5d17da2afac
I_am_Vietnam <91591390+ImVietnam@users.noreply.github.com>
2023-06-07 15:42:45 +0700

    Update test.js

--- 1-js/03-code-quality/05-testing-mocha/beforeafter.view/test.js
+++ 1-js/03-code-quality/05-testing-mocha/beforeafter.view/test.js
@@ -2,9 +2,9 @@ describe("test", function() {
   
    // Mocha thường đợi các bài kiểm tra trong 2 giây trước khi coi chúng sai
   
-  this.timeout(200000); // Với mã này, chúng ta tăng mã này - trong trường hợp này là 200.000 mili giây
+  this.timeout(200000); // Với mã này, chúng ta tăng cái này (timeout) - trong trường hợp này là 200.000 mili giây
 
-  // Điều này là do chức năng "cảnh báo", bởi vì nếu bạn trì hoãn nhấn nút "OK" thì các bài kiểm tra sẽ không vượt qua!
+  // Điều này là do hàm "alert", bởi vì nếu bạn trì hoãn nhấn nút "OK" thì các bài kiểm tra sẽ không vượt qua!
   
   before(() => alert("Bài kiểm tra bắt đầu – trước tất cả các bài kiểm tra"));
   after(() => alert("Bài kiểm tra kết thúc – sau tất cả các bài kiểm tra"));
