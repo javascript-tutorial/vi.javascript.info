6ef417213fc162600a7e28e267436a34c874e5df
I_am_Vietnam <91591390+ImVietnam@users.noreply.github.com>
2023-06-12 10:25:06 +0700

    Update solution.js

--- 1-js/05-data-types/05-array-methods/1-camelcase/_js.view/solution.js
+++ 1-js/05-data-types/05-array-methods/1-camelcase/_js.view/solution.js
@@ -1,10 +1,10 @@
 function camelize(str) {
   return str
-    .split('-') // tách 'my-long-word' thành array ['my', 'long', 'word']
+    .split('-') // tách 'từ-dài-của-tôi' thành array ['từ', 'dài', 'của', 'tôi']
     .map(
       // viết hoa các chữ cái đầu tiên của tất cả các phần tử array ngoại trừ phần tử đầu tiên
-       // chuyển ['my', 'long', 'word'] thành ['my', 'Long', 'Word']
+       // chuyển ['từ', 'dài', 'của' 'tôi'] thành ['từ', 'Dài', 'Của', 'Tôi']
       (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
     )
-    .join(''); // nối ['my', 'Long', 'Word'] thành 'myLongWord'
+    .join(''); // nối ['từ', 'Dài', 'Của', 'Tôi'] thành 'từDàiCủaTôi'
 }
